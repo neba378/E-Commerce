b32cff7ef4ef54fe468c3a23a2c0eefa96a593a6
Nebiyu Musbah <nebiyumusbah378@gmail.com>
2024-12-23 14:51:45 +0300

    add registration functionality

--- api/routes/User.js
+++ api/routes/User.js
@@ -7,29 +7,50 @@ const User = require("../models/User");
 userRoute.post(
   "/login",
   AsyncHandler(async (req, res) => {
-    const {email, password} = req.body;
+    const { email, password } = req.body;
 
-    const user = await User.findOne({email})
+    const user = await User.findOne({ email });
 
-    if (user && (await user.matchPassword(password))){
-
-        res.json({
-            _id: user.id,
-            name: user.name,
-            email: user.email,
-            isAdmin: user.isAdmin,
-            token: null,
-            createdAt: user.createdAt
-
-        })
-    }else{
-        res.status(401)
-        throw new Error("Invalid email")
+    if (user && (await user.matchPassword(password))) {
+      res.json({
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: null,
+        createdAt: user.createdAt,
+      });
+    } else {
+      res.status(401);
+      throw new Error("Invalid email");
     }
-
-
   })
 );
 
+userRoute.post('/', 
+    AsyncHandler(async(req,res)=>{
+        const {name, email, password} = req.body;
+        const existUser = await User.findOne({email:email})
+        if (existUser){
+            res.status(400)
+            throw Error("User already exists")
+        }
 
-module.exports = userRoute
+        const user = await User.create({
+            name,
+            email,
+            password
+        })
+        if (user) {
+            res.status(200).json({
+                _id:user._id,
+                name:user.name,
+                email:user.email,
+                isAdmin:user.isAdmin,
+                createdAt:user.createdAt
+            })
+        }
+    })
+)
+
+module.exports = userRoute;
