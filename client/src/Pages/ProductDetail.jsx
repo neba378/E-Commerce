5f04eacce08c2d9bdfbbc3af4efbf8b5451cbbbf
Nebiyu Musbah <nebiyumusbah378@gmail.com>
2024-12-26 11:17:51 +0300

    add routing

---
+++ client/src/Pages/ProductDetail.jsx
@@ -0,0 +1,14 @@
+import Layout from "./../Layouts/Layout";
+import Detail from "../components/Detail";  
+function ProductDetail() {
+  return (
+    <>
+    
+      <Layout>
+        <Detail />
+      </Layout>
+    </>
+  );
+}
+
+export default ProductDetail;
